a0712d9812b4469dadc28ca63940cc81be947db8
tal21-meet <tal21@meet.mit.edu>
2019-07-21 11:08:35 -0400

    Stopping here

---
+++ MEETinTurtle.py
@@ -0,0 +1,52 @@
+import turtle 
+
+# Everything that comes after the # is a 
+# comment.
+# It is a note to the person reading the code.
+# The computer ignores it.
+# Write your code below here...
+turtle.penup() #Pick up the pen so it doesn’t 
+               #draw
+turtle.goto(-200,-100) #Move the turtle to the 
+ #position (-200, -100) 
+ #on the screen
+turtle.pendown() #Put the pen down to start
+                 #drawing
+#Draw the M:
+turtle.goto(-200,-100+200) 
+turtle.goto(-200+50,-100) 
+turtle.goto(-200+100,-100+200)
+turtle.goto(-200+100,-100)
+turtle.penup()
+turtle.goto(-150+100,-100)
+turtle.pendown()
+turtle.goto(-50+100,-100)
+turtle.penup()
+turtle.goto(-50+100,-100+200)
+turtle.pendown()
+turtle.goto(-150+100,-100+200)
+turtle.goto(-150+100,-100+100)
+turtle.goto(-50+100,-100+100)
+turtle.goto(-150+100,-100+100)
+turtle.goto(-150+100,-100)
+turtle.penup()
+turtle.goto(-150+250,-100)
+turtle.pendown()
+turtle.goto(-50+250,-100)
+turtle.penup()
+turtle.goto(-50+250,-100+200)
+turtle.pendown()
+turtle.goto(-150+250,-100+200)
+turtle.goto(-150+250,-100+100)
+turtle.goto(-50+250,-100+100)
+turtle.goto(-150+250,-100+100)
+turtle.goto(-150+250,-100)
+turtle.penup()
+turtle.goto(-150+400,100)
+turtle.pendown()
+turtle.goto(-150+500,100)
+turtle.goto(-150+450,100)
+turtle.goto(-150+450,-100)
+# turtle.mainloop() tells the turtle to do all
+# the turtle commands above it and paint it on the screen.
+# It always has to be the last line of code!
